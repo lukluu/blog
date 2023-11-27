13c99894b98fe1a3c57a886d912d946f3be87df3
zahravebryanmaharani <vebryanzahra@gmail.com>
2023-11-27 22:17:09 +0800

    Membuat Route Post, Controller, Mengisi tabel post dan Mmebuat file view post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,22 +30,22 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'taufik@gmail.com',
         //     'password' => bcrypt('123')
         // ]);
-        Category::create([
-            'name' => 'Web Programming',
-            'slug' => 'web-programming'
-        ]);
-        Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-        Category::create([
-            'name' => 'Engineer',
-            'slug' => 'engineer'
-        ]);
-        Saya::create([
-            'nama' => 'lukman',
-            'nim' => 'E1E121004',
-            'jurusan' => 'Informatika'
-        ]);
+        // Category::create([
+        //     'name' => 'Web Programming',
+        //     'slug' => 'web-programming'
+        // ]);
+        // Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
+        // Category::create([
+        //     'name' => 'Engineer',
+        //     'slug' => 'engineer'
+        // ]);
+        // Saya::create([
+        //     'nama' => 'lukman',
+        //     'nim' => 'E1E121004',
+        //     'jurusan' => 'Informatika'
+        // ]);
     }
 }
